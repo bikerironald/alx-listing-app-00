f14330fee6a302a279c89edc07830f81772f3164
bikerironald <bikerironald@gmail.com>
2025-06-03 01:37:42 +0300

    Build Responsive Listing Page with Tailwind CSS

---
+++ interfaces/index.ts
@@ -0,0 +1,20 @@
+// ...existing code...
+
+export interface PropertyProps {
+  name: string;
+  address: {
+    state: string;
+    city: string;
+    country: string;
+  };
+  rating: number;
+  category: string[];
+  price: number;
+  offers: {
+    bed: string;
+    shower: string;
+    occupants: string;
+  };
+  image: string;
+  discount: string;
+}
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PagesIndex = () => {
+  return (
+    <div>PagesIndex</div>
+  )
+}
+
+export default PagesIndex
